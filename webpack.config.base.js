c9597cf2a1524eb34c280b2a0b7a9ed86af7cd2e
sunee <1250432262@qq.com>
2022-02-10 16:50:50 +0800

    config merge

---
+++ webpack.config.base.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App", //即html的<title>部分
+      template: "src/assets/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [],
+  },
+};
