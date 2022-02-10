c727cb881bf94b0ba6e6e3513bec3056e86015dc
sunee <1250432262@qq.com>
2022-02-10 15:33:58 +0800

    抽离css

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
   },
   devtool: "inline-source-map",
   // devServer: {
@@ -16,12 +17,19 @@ module.exports = {
       title: "My App", //即html的<title>部分
       template: "src/assets/index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+      ignoreOrder: false,
+    }),
   ],
+
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        // use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
